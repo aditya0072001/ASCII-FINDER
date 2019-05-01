32d3c7efee05d06c7a02cfd25aad3a80aaae60f1
Aditya Tripathi <41997646+aditya0072001@users.noreply.github.com>
2019-05-01 15:20:42 +0530

    Many improvements

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,19 @@
 package tripathi.aditya.asciifinder;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -14,6 +21,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     EditText input;
+    TextView value;
     Button ok;
     char ch;
     String h;
@@ -25,17 +33,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         input=(EditText) findViewById(R.id.input);
         ok=(Button)findViewById(R.id.ok);
+        value=(TextView)findViewById(R.id.value);
+
+       /* input.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction()==KeyEvent.ACTION_DOWN){
+                    switch (keyCode)
+                    {
+                        case KeyEvent.KEYCODE_ENTER:
+                            InputMethodManager imm =(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                            if(imm.isAcceptingText()){
+                                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+                            }
+                    }
+                }
+                return false;
+            }
+        });
+        */
 
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 h=input.getText().toString();
-                ascii[0]=(int) h.charAt(0);
+                if(h.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Enter character", Toast.LENGTH_SHORT).show();
+                }else{
+                    ascii[0]=(int) h.charAt(0);
 
-               input.setText(Arrays.toString(ascii));
+                    value.setText("ASCII value :"+Arrays.toString(ascii));
+                    input.setText("");
+                }
+
             }
         });
 
